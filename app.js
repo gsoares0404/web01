3e342fd599e6a11417bd001b292182edcef2bff8
Gabriel Costa Soares <102666870+gsoares0404@users.noreply.github.com>
2022-08-16 21:53:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let line1 = document.querySelector('.line-1');
-let line2 = document.querySelector('.line-2');
+let line1 = document.querySelector('.line-1')
+let line2 = document.querySelector('.line-2')
 
 window.onscroll = () =>{
     let pos = window.scrollY - 1100;
@@ -7,3 +7,21 @@ window.onscroll = () =>{
     line2.style.right = `${pos}px`
      
 }
+
+const yourName = document.getElementById('name')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('errorname')
+const email = document.getElementById('email')
+const errorEmail = document.getElementById('erroremail')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (yourName.value === '' || yourName.value == null) {
+    messages.push('Name ist ungultig')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
